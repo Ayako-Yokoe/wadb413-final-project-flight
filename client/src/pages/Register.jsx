7eabde07acda46137bac4902fc4337d32cff585d
Ayako-Yokoe <niluvsuall@gmail.com>
2022-02-20 20:34:37 -0800

    new mongodb

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -68,7 +68,7 @@ const Register = () => {
       e.preventDefault()
       setIsSubmitting(true)
       setError('')
-
+      
       fetch(process.env.REACT_APP_API_ENDPOINT + 'api/v1/auth/signup', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
