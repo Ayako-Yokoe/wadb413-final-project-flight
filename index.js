5b8835e17b95d8246d8bca5888276b317e8ba5b0
Ayako-Yokoe <niluvsuall@gmail.com>
2022-02-23 15:55:43 -0800

    heroku frontend side

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require("path");
 
 require('dotenv').config()
 require('express-async-errors')
@@ -7,18 +8,23 @@ require('./util/db')
 
 const app = express()
 
+// Francois's help
+app.use(express.static(path.resolve(__dirname, "./client/build"))); 
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+}); 
+
+
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/v1', require('./routes/index'))
 
-app.get('/', (req, res, next) => {
-    res.status(200).json({ status: 'success' });
-});
 
 app.use((error, req,res,next) => {
     res.status(500).json({ error: error.message })
 })
+
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log('Listening to port : ', PORT))
