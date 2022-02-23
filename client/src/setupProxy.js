d54260bb5fcbd8f569cc54748c62dcd20740a356
Ayako-Yokoe <niluvsuall@gmail.com>
2022-02-22 23:22:27 -0800

    changed proxy url

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,8 @@ const { createProxyMiddleware} = require('http-proxy-middleware')
 module.exports = function(app){
     app.use(
         ['api/v1', '/api/*'],
-        createProxyMiddleware({ target: "http://localhost:8080"})
+        // createProxyMiddleware({ target: "http://localhost:8080"})
+        createProxyMiddleware({ target: "https://final-project-flight.herokuapp.com/"})
     )
 }
 
